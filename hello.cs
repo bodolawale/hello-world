8db3b44de636f90221820906d47205967ceed125
bodolawale <bodolawale@gmail.com>
2017-05-21 18:24:40 +0100

    hello-world.cs

---
+++ hello.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class program
+{
+	public static void Main()
+	{
+		for(int i = 0; i < 10; i++)
+		{
+			Console.WriteLine("And so it begins "+i);
+		}
+
+		//Console.ReadLine();		
+
+	}
+}
